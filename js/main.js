f6440db56a682c7798de53c96f39d717a430682d
twotimesgi <luigibardellagerbi@mail.com>
2022-03-09 15:42:07 +0100

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 let containerHTML = document.getElementById("js-container");
+let categories = [];
+let selectHTML = document.getElementById("select-category");
+renderHTLM();
+getCategories();
 
-for (let i = 0; i < iconsDB.length; i++) {
-    let item = document.createElement("div");
-    item.classList.add("item");
-    item.innerHTML = `<i style="color: ${iconsDB[i].color};" class="${iconsDB[i].family} ${iconsDB[i].prefix+iconsDB[i].name}"></i>
-                      <span>${iconsDB[i].name}</span>`;
-    containerHTML.append(item);
+selectHTML.addEventListener("click", function(){
+    
+});
+
+function renderHTLM(){
+    for (let i = 0; i < iconsDB.length; i++) {
+        let item = document.createElement("div");
+        item.classList.add("item");
+        item.innerHTML = `<i style="color: ${iconsDB[i].color};" class="${iconsDB[i].family} ${iconsDB[i].prefix+iconsDB[i].name}"></i>
+                        <span>${iconsDB[i].name}</span>`;
+        containerHTML.append(item);
+    }
 }
 
+function getCategories(){
+    iconsDB.forEach((icon) => {
+        if(!categories.includes(icon.type)){
+             categories.push(icon.type);
+             let option = document.createElement("option");
+             option.value = icon.type;
+             option.innerHTML = icon.type;
+             selectHTML.append(option);
+        }
+    });
+}
