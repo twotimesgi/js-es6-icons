d150dfb64960a55315b8a9f2b1b5f1f5a2ff790b
twotimesgi <luigibardellagerbi@mail.com>
2022-03-09 16:24:06 +0100

    Bonus colori

--- js/main.js
+++ js/main.js
@@ -1,8 +1,124 @@
+let iconsDB = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: null
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: null
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: null
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: null
+	}
+];
+
 let containerHTML = document.getElementById("js-container");
 let categories = ["All"];
 let selectHTML = document.getElementById("select-category");
-renderHTLM(...iconsDB);
-getCategories();
+assignColor(iconsDB);
+renderHTLM(iconsDB);
+getCategories(iconsDB);
 
 selectHTML.addEventListener("change", function(e){
     filterBy(selectHTML.value);
@@ -16,11 +132,11 @@ function filterBy(category){
     }else{
     filteredIcons = iconsDB;
     }
-    
-    renderHTLM(...filteredIcons); 
+
+    renderHTLM(filteredIcons); 
 }
 
-function renderHTLM(...arr){
+function renderHTLM(arr){
     containerHTML.innerHTML = "";    
     for (let i = 0; i < arr.length; i++){
         let item = document.createElement("div");
@@ -31,8 +147,8 @@ function renderHTLM(...arr){
     }
 }
 
-function getCategories(){
-    iconsDB.forEach((icon) => {
+function getCategories(arr){
+    arr.forEach((icon) => {
         if(!categories.includes(icon.type)){
              categories.push(icon.type);
              let option = document.createElement("option");
@@ -41,4 +157,14 @@ function getCategories(){
              selectHTML.append(option);
         }
     });
+}
+
+
+function assignColor(arr){
+	arr.forEach(icon => {
+		if(icon.color == null){
+			let randomColor = Math.floor(Math.random()*16777215).toString(16);
+			icon.color = "#" + randomColor;
+		}
+	});
 }
